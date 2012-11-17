ef4a216f95ea1e00e62065299f35ea514d93021a
gtx670 <soulsheng@qq.com>
2012-11-17 17:22:06 +0800

    先读后写问题，指令瓶颈条件下，concurrent还是不能提升效率，不会使用。

--- cuda-sdk-samples/src/concurrentKernels/concurrentKernels.cu
+++ cuda-sdk-samples/src/concurrentKernels/concurrentKernels.cu
@@ -28,9 +28,9 @@ const char *sSDKsample = "concurrentKernels";
 #define		IPL				1// 1 2 4 8
 #define SIZE_ELEMENT		(1<<20)// 1M£¬Ò»°ÙÎå
 #define SIZE_BLOCK			(1024/IPL)
-#define		SIZE_KERNEL		1// 1 2 4 8
+#define		SIZE_KERNEL		4// 1 2 4 8
 #define SIZE_SHARED_MEMORY	((1<<10)*1)  
-#define INSTRUCTION_MAX		(100)
+#define INSTRUCTION_MAX		(1<<20)
 
 // This is a kernel that does no real work but runs at least for a specified number of clocks
 __global__ void clock_block(clock_t* d_o, clock_t clock_count)
@@ -350,7 +350,7 @@ int main(int argc, char **argv)
 
 		dim3  grid( nSizeGridX/nkernels, nSizeGridY, 1);
 		dim3  threads( nSizeBlock, 1, 1);
-#if 0	
+#if 1	
     cudaEventRecord(start_event, 0);
     // queue nkernels in separate streams and record when they are done
     for( int i=0; i<nkernels; ++i)
@@ -358,7 +358,7 @@ int main(int argc, char **argv)
 		// setup execution parameters
 
         //clock_block<<<1,1,0,streams[i]>>>(&d_a[i], time_clocks );
-		testReadThenWrite<<<grid,threads,SIZE_SHARED_MEMORY,streams[i]>>>( d_idata+i*SIZE_ELEMENT/nkernels, d_odata+i*SIZE_ELEMENT/nkernels );
+		testReadThenWriteInstMax<<<grid,threads,SIZE_SHARED_MEMORY,streams[i]>>>( d_idata+i*SIZE_ELEMENT/nkernels, d_odata+i*SIZE_ELEMENT/nkernels );
 
         cudaEventRecord(kernelEvent[i], streams[i]) ;
 	
@@ -372,8 +372,10 @@ int main(int argc, char **argv)
     // in this sample we just wait until the GPU is done
 	cudaEventRecord(stop_event, 0) ;
 	cudaEventSynchronize(stop_event) ;
-    cudaEventElapsedTime(&elapsed_time, start_event, stop_event) ;
+    cudaEventElapsedTime(&kernel_time, start_event, stop_event) ;
 #endif
+
+	grid.x *= nkernels;
 	cudaEventRecord(start_event, 0);
 	testReadThenWriteInstMax<<<grid,threads>>>( d_idata, d_odata );
 	cudaEventRecord(stop_event, 0) ;
@@ -381,8 +383,8 @@ int main(int argc, char **argv)
 	cudaEventElapsedTime(&elapsed_time, start_event, stop_event) ;
 
 	//shrLog("Expected time for serial execution of %d kernels = %.3fms\n", nkernels, nkernels * kernel_time );
-	//shrLog("Expected time for concurrent execution of %d kernels = %.3fms\n", nkernels, kernel_time );
-	shrLog("Measured time for sample = %.3fms\n", elapsed_time );
+	shrLog("Measured time for concurrent execution of %d kernels = %.3fms\n", nkernels, kernel_time );
+	shrLog("Measured time for no concurrent = %.3fms\n", elapsed_time );
 
     // release resources
     for(int i = 0; i < nkernels; i++) {
